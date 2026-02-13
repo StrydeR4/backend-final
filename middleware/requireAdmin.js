550445b3c475b681a29e5ee3ec672c71db746d76
Ruslan <104831187+StrydeR4@users.noreply.github.com>
2026-02-13 16:18:24 +0500

    Delete middleware directory

--- middleware/requireAdmin.js
+++
@@ -1,6 +0,0 @@
-module.exports = function requireAdmin(req, res, next) {
-  if (!req.user || req.user.role !== "admin") {
-    return res.status(403).json({ message: "Admin only" });
-  }
-  next();
-};
